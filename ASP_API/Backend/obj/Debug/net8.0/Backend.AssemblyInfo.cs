d7d830d08a956d91aa0e3a0242e8f3c7dd464e60
Raphael Lucas <rafaoliveira2009@Outlook.com>
2026-05-08 09:03:49 -0300

    Iniciar template

--- ASP_API/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ ASP_API/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a48c0ba15e30c462848d578c5cca256081d2614")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96ab3bea20854b249ad9cd74fbacd109477a14b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
